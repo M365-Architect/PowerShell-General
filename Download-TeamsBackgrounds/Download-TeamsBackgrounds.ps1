68e8320b900bc3a9a599543d53dc9f69773c26d4
Andreas Hähnel <andreas.haehnel@infowan.de>
2021-05-20 13:21:38 +0200

    Update Download-TeamsBackgrounds.ps1
    Download script finalized and tested

--- Download-TeamsBackgrounds/Download-TeamsBackgrounds.ps1
+++ Download-TeamsBackgrounds/Download-TeamsBackgrounds.ps1
@@ -1,1 +1,18 @@
-# .
+Clear-Host
+$ErrorActionPreference = "SilentlyContinue"
+$numberOfBackgrounds = 20
+$targetFolder = "C:\tmp\Teams Backgrounds\"
+$FilenameStart = "https://adoption.azureedge.net/wp-content/custom-backgrounds-gallery/user-submitted-background-"
+#$FilenameStart = "https://adoption.azureedge.net/wp-content/custom-backgrounds-gallery/VIVA-background-Abstract-0"
+#$FilenameStart = "https://adoption.azureedge.net/wp-content/custom-backgrounds-gallery/VIVA-background-office-0"
+#$FilenameStart = "https://adoption.azureedge.net/wp-content/custom-backgrounds-gallery/VIVA-background-home-0"
+$FilenameFormat = ".jpg"
+
+1..$numberOfBackgrounds | ForEach-Object{$url = $FilenameStart + $_.ToString() + $FilenameFormat; Invoke-WebRequest -Uri $url -OutFile( $targetFolder + $url.Split("/")[$url.Split("/").length - 1] ) -UseBasicParsing -ErrorAction 'SilentlyContinue'}
+
+#copy to Teams images folder
+$list = Get-ChildItem $targetFolder
+foreach($file in $list)
+{
+    Copy-Item $file.FullName -Destination ($env:APPDATA + "\Microsoft\Teams\backgrounds\")
+}
