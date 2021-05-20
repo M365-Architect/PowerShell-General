b95c450e9606914342cf71b710101f812e941581
Andreas Hähnel <andreas.haehnel@infowan.de>
2021-05-20 13:28:46 +0200

    Update Download-TeamsBackgrounds.ps1
    added finalizing message

--- Download-TeamsBackgrounds/Download-TeamsBackgrounds.ps1
+++ Download-TeamsBackgrounds/Download-TeamsBackgrounds.ps1
@@ -8,7 +8,8 @@ $FilenameStart = "https://adoption.azureedge.net/wp-content/custom-backgrounds-g
 #$FilenameStart = "https://adoption.azureedge.net/wp-content/custom-backgrounds-gallery/VIVA-background-home-0"
 $FilenameFormat = ".jpg"
 
-1..$numberOfBackgrounds | ForEach-Object{$url = $FilenameStart + $_.ToString() + $FilenameFormat; Invoke-WebRequest -Uri $url -OutFile( $targetFolder + $url.Split("/")[$url.Split("/").length - 1] ) -UseBasicParsing -ErrorAction 'SilentlyContinue'}
+Write-Host "Downloading background images " -NoNewline
+1..$numberOfBackgrounds | ForEach-Object{$url = $FilenameStart + $_.ToString() + $FilenameFormat; Invoke-WebRequest -Uri $url -OutFile( $targetFolder + $url.Split("/")[$url.Split("/").length - 1] ) -UseBasicParsing -ErrorAction 'SilentlyContinue'; Write-Host "." -NoNewline}
 
 #copy to Teams images folder
 $list = Get-ChildItem $targetFolder
@@ -16,3 +17,5 @@ foreach($file in $list)
 {
     Copy-Item $file.FullName -Destination ($env:APPDATA + "\Microsoft\Teams\backgrounds\Uploads")
 }
+
+Write-Host "`n`nNew background images successfully saved. Please restart Teams to use the backgrounds." -ForegroundColor Magenta
